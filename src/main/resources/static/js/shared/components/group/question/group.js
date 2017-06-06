77b5be71a70b9f67cdcbcdb5bc82f3c94ac9bfb0
alvesrocha <neoarc@gmail.com>
2017-06-07 01:22:28 +0200

    improvements

--- src/main/resources/static/js/shared/components/group/question/group.js
+++
@@ -1,9 +0,0 @@
-function GroupController() {
-  var ctrl = this;
-  //for now the group doesn't need any behaviour
-}
-
-angular.module('form').component('group', {
-  templateUrl: 'group.html',
-  controller: GroupController
-});
